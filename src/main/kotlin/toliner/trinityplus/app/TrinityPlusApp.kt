27e9946006bf2230c4465bd59bac547f9ad457e7
toliner <toliner.rkt@gmail.com>
2019-03-22 03:41:06 +0900

    :heavy_plus_sign: App初期化時のConfigの読み出しの追加

--- src/main/kotlin/toliner/trinityplus/app/TrinityPlusApp.kt
+++ src/main/kotlin/toliner/trinityplus/app/TrinityPlusApp.kt
@@ -2,5 +2,20 @@ package toliner.trinityplus.app
 
 import toliner.trinityplus.view.MainView
 import tornadofx.*
+import java.io.File
+import java.nio.file.Paths
 
-class TrinityPlusApp : App(MainView::class)
+class TrinityPlusApp : App(MainView::class) {
+
+    override val configBasePath = Paths.get(System.getProperty("user.home"), "Trinity+")!!.also {
+        it.toFile().apply {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+    }
+
+    init {
+        File(config.string("game_directory", configBasePath.toAbsolutePath().toString())).mkdirs()
+    }
+}
